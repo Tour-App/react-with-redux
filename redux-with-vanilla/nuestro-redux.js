f2fea4e3e5741c27a71b778efbb9de722ae65f43
Misael Calvillo Mancilla <misa@homely.mx>
2022-05-25 20:27:26 -0500

    Crear version inicial del nuestro redux

--- redux-with-vanilla/nuestro-redux.js
+++ redux-with-vanilla/nuestro-redux.js
@@ -1,1 +1,62 @@
 console.log('Saludos desde nuestro redux');
+
+// let action = {
+//   type: 'ADD_TODO',
+//   todo: 'Terminar el store de redux'
+// }
+
+function createStore(reducer) {
+
+  // Un store debe tener cuatro partes
+  // 1. El estado ✅
+  // 2. Obtener el estado ✅
+  // 3. Escuchar cambios al estado ✅
+  // 4. Actualizar el estado ✅
+
+  let state // Variable privada
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  }
+}
+// El REDUCER
+function todos(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  if (action.type === 'ERRASE_TODOS') {
+    return state = [];
+  }
+
+  return state;
+}
+
+const store = createStore(todos);
+
+store.subscribe(() => {
+  console.log('El nuevo estado es: ', store.getState());
+})
+
+const unsubscribe = store.subscribe(() => {
+  console.log('El store cambió');
+})
+
+// unsubscribe();
